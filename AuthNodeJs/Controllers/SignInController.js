fb4e7a6f93376d8ed09d12e0bbf905b8ab8b2cb3
hamzahafaiedh <hamza.hafaiedh@esprit.tn>
2023-10-31 22:07:07 +0100

    authNodejs

---
+++ AuthNodeJs/Controllers/SignInController.js
@@ -0,0 +1,56 @@
+const { response } = require("express");
+const User = require("../Models/User");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+
+
+// create json web token
+const maxAge = 15 * 60
+
+//function createToken the encoded data here is the id
+const createToken = (id) => {
+    return jwt.sign({ id }, 'Hamza :)', {    //secret key that is used to sign the jwt (should not share)
+        expiresIn: maxAge
+    })
+}
+
+
+
+const SignIn = async (req, res) => {
+    const saltRounds = 10;
+    const { email, password } = req.body;
+    if(!email || !password){
+        res.status(500).json("empty fields")
+    }
+    else{
+    try {
+        const user = await User.findOne({ email: email });
+        const passwordMatches = await bcrypt.compare(password, user.password);
+        if (user) {
+            if ( passwordMatches) {
+                const token1 = createToken(user._id);
+                    // Update the user's token and get the updated document
+                    const updatedUser = await User.findOneAndUpdate(
+                        { email: email },
+                        { $set: { token: token1 } },
+                        { new: true } // This option returns the updated document
+                    );
+
+                    res.status(200).json(updatedUser);
+            }
+            else {
+                res.status(500).json("check your password");
+            }
+        }
+        else {
+            res.status(500).json("this email does not exist");
+        }
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+}
+module.exports={SignIn};
+
+
