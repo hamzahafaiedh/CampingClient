41bd5c4d8317badcd5e99bc88bcaffe9d9eb751e
MariemKhadhraoui <117642592+MariemKhadhraoui@users.noreply.github.com>
2023-11-01 07:34:04 +0100

    Avis CRUD

--- CampinClient/src/App.js
+++ CampinClient/src/App.js
@@ -5,6 +5,9 @@ import { Routes , Route } from 'react-router-dom';
 import Home from './pages/Home/index'
 import AddReservation from './pages/Reservation/AddReserVation';
 import ListReservations from './pages/Reservation/ListReservations';
+import AddAvis from './pages/Avis/AddAvis';
+import ListAvis from './pages/Avis/ListAvis';
+import Updatereview from './pages/Avis/UpdateAvis';
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
       <Route path='/Signin' element={<Signin></Signin>} ></Route>
       <Route path='/AddReservation' element={<AddReservation></AddReservation>} ></Route>
       <Route path='/ListReservations' element={<ListReservations></ListReservations>} ></Route>
+      <Route path='/AddAvis' element={<AddAvis></AddAvis>} ></Route>
+      <Route path='/ListAvis' element={<ListAvis></ListAvis>} ></Route>
+      <Route path='/updateavis/:idAvis' element={<Updatereview></Updatereview>} ></Route>
       
     </Routes> 
   
