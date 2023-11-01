df220b5450e28e4ea62572a7bd89660d326d5dcc
[oussama-gadria] <gadria.oussama@esprit.tn>
2023-11-01 07:45:17 +0100

    feature frontside

--- CampinClient/src/App.js
+++ CampinClient/src/App.js
@@ -5,6 +5,9 @@ import { Routes , Route } from 'react-router-dom';
 import Home from './pages/Home/index'
 import AddReservation from './pages/Reservation/AddReserVation';
 import ListReservations from './pages/Reservation/ListReservations';
+import AddCamping from './pages/Camping/AddCamping';
+import ListCamping from './pages/Camping/ListCamping';
+import UpdateCamping from './pages/Camping/UpdateCamping';
 
 function App() {
   return (
@@ -15,7 +18,10 @@ function App() {
       <Route path='/Signup' element={<Signup></Signup>} ></Route>
       <Route path='/Signin' element={<Signin></Signin>} ></Route>
       <Route path='/AddReservation' element={<AddReservation></AddReservation>} ></Route>
+      <Route path='/AddCamping' element={<AddCamping></AddCamping>} ></Route>
+        <Route path='/Campings' element={<ListCamping></ListCamping>} ></Route>
       <Route path='/ListReservations' element={<ListReservations></ListReservations>} ></Route>
+      <Route path='/UpdateCamping/:campingId' element={<UpdateCamping></UpdateCamping>} ></Route>
       
     </Routes> 
   
